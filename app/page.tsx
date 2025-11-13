bb52ec060a019200fbcb719b82a8e716bc84c8b5
Chetan Sharma <chetan@tintbytes.com>
2025-11-13 20:39:26 +0530

    changed dashboard route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
-import Landings from "@/components/custom/Landings";
-import MarketPrices from "@/components/custom/MarketPrices";
-import MarketSignals from "@/components/custom/MarketSignals";
-import QuotaUsage from "@/components/custom/QuotaUsage";
 import { getToken } from "../utils/lib/auth";
 import { useEffect } from "react";
 
